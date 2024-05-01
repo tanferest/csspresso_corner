2326e966c4508e3aafd2fe1bdb81de4dac00747f
tanferest <tanferest@gmail.com>
2024-05-01 17:36:02 +0200

    homepage, mobile version done

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -9,7 +9,7 @@ function Homepage () {
             ¡Bienvenidos a CSSpresso Corner! Sumérgete en una experiencia sensorial única donde el aroma del café recién tostado se mezcla con el cálido ambiente de nuestra cafetería. En cada taza de nuestro exquisito café, encontrarás el equilibrio perfecto entre pasión, calidad y creatividad. Descubre un lugar donde cada sorbo es una aventura, cada bocado es un deleite y cada encuentro es una conexión. ¡Ven y déjate envolver por la magia de CSSpresso Corner, donde cada momento se convierte en un placer inolvidable!
             </p>
         </div>
-        <button className='btn homepage__btn--1'>Ver carta</button>
+        <a className='btn homepage__btn--1'>Ver carta</a>
         <button className='btn homepage__btn--2'>Conócenos</button>
     </main>
 };
